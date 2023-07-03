9c566f2a5ad1f8821cb11a79b253f031c02fbf07
Razor <72689902+RazorTheWolf@users.noreply.github.com>
2023-07-03 20:14:07 +0200

    Changed logs from print to fatal

--- utils/exchange.go
+++ utils/exchange.go
@@ -34,21 +34,21 @@ func Exchange(code string) (OAuth2, int, string) {
 		os.Getenv("SCOPE"))
 	res, err := http.Post(BaseURL+"oauth2/token", "application/x-www-form-urlencoded", strings.NewReader(body))
 	if err != nil {
-		log.Print("Error with the POST request", err)
+		log.Fatal("Error with the POST request", err)
 	}
 	var b []byte
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Print("Error while trying to close body", err)
+			log.Fatal("Error while trying to close body", err)
 		}
 	}(res.Body)
 	if b, err = io.ReadAll(res.Body); err != nil {
-		log.Print("Error reading response body", err)
+		log.Fatal("Error reading response body", err)
 	}
 	err = json.Unmarshal(b, &oauth2)
 	if err != nil {
-		log.Print("Error while parsing data to struct", err)
+		log.Fatal("Error while parsing data to struct", err)
 	}
 	return oauth2, res.StatusCode, string(b)
 }
